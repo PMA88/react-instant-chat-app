777215271ca59eaeadb17b0a2626191e2b2f7788
Alan Paton <alanbpaton@gmail.com>
2021-07-28 22:02:24 +0200

    Adding chatbox components and displays

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,6 +5,9 @@ import io from "socket.io-client";
 import "./Chat.css";
 
 import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+
 let socket;
 
 const Chat = ({ location }) => {
@@ -52,13 +55,12 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
-        {/* <input
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={(event) =>
-            event.key === "Enter" ? sendMessage(event) : null
-          }
-        /> */}
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
       </div>
     </div>
   );
